0dffe8f486f633e05bff70f014e2715c32a4fef0
Sarapessoa <sarapessoa.2002.09@gmail.com>
2023-05-22 10:31:50 -0300

    alteracoes

--- index.js
+++ index.js
@@ -10,24 +10,25 @@ dotenv.config();
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
 
+const app = express();
+app.use(express.json());
+app.use(cors({
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization'
+  }));
+
+app.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    return res.status(200).json({msg: "Rota pública"});
+})
+app.use('/auth', authRoutes);
+app.use('/usuario', usuarioRoutes);
+
+app.listen(3000);
+
 mongoose.connect(
     `mongodb+srv://${dbUser}:${dbPass}@cluster0.whxr4yj.mongodb.net/?retryWrites=true&w=majority`
 )
 .then(() => {
-    const app = express();
-    app.use(express.json());
-    app.use(cors({
-        methods: 'GET,POST,PUT,DELETE',
-        allowedHeaders: 'Content-Type,Authorization'
-      }));
-
-    app.get('/', (req, res) => {
-        res.setHeader('Content-Type', 'application/json');
-        return res.status(200).json({msg: "Rota pública"});
-    })
-    app.use('/auth', authRoutes);
-    app.use('/usuario', usuarioRoutes);
-
-    app.listen(3000);
     console.log("Servidor Iniciado e Conectado ao Banco")
 }).catch((error) => console.log(error))
